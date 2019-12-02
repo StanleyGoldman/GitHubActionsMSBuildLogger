b972eaf4697dfadb2805b2e1aaf4e38a1b6d0ee2
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-12-02 10:59:13 -0500

    Initial commit

---
+++ test-resources/roslynator/TestConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Main
+    {
+        static void Hello()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
---
+++ test-resources/simple-project-info/TestConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Adding some comment here for an informational item
+            //Adding some comment here for an informational item
+        }
+    }
+}
