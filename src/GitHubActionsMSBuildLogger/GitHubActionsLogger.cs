e13c22969deb2876da97d362ced668c36ce1bca8
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-12-02 12:10:01 -0500

    Adding a bunch of basics

---
+++ src/GitHubActionsMSBuildLogger/GitHubActionsLogger.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace GitHubActionsMSBuildLogger
+{
+    public class GitHubActionsLogger : Logger
+    {
+        public override void Initialize(IEventSource eventSource)
+        {
+            eventSource.MessageRaised += (sender, args) => { };
+            eventSource.WarningRaised += (sender, args) => { };
+            eventSource.ErrorRaised += (sender, args) => { };
+        }
+    }
+}
