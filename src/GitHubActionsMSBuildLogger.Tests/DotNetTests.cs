92360be74d64490e03a465ade6d933e826a3a84e
Stanley Goldman <Stanley.Goldman@gmail.com>
2019-12-02 18:15:37 -0500

    Getting it right

--- src/GitHubActionsMSBuildLogger.Tests/DotNetTests.cs
+++ src/GitHubActionsMSBuildLogger.Tests/DotNetTests.cs
@@ -1,50 +1,57 @@
+using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using RunProcessAsTask;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GitHubActionsMSBuildLogger.Tests
 {
     public class DotNetTests : BuildTestsBase
     {
+        public DotNetTests(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+
+        private readonly ITestOutputHelper _output;
+
+        private async Task<ProcessResults> BuildAsync(string simpleProjectInfo)
+        {
+            var loggerPath = GetLoggerPathOrThrow();
+            var slnPath = GetSolutionPathOrThrow(simpleProjectInfo);
+
+            var processStartInfo =
+                new ProcessStartInfo("dotnet", $"build {slnPath} /logger:GitHubActionsLogger,{loggerPath}")
+                {
+                    Environment =
+                    {
+                        {"GITHUB_ACTION", Guid.NewGuid().ToString()},
+                        {"GITHUB_WORKSPACE", TargetPath}
+                    }
+                };
+
+            return await ProcessEx.RunAsync(processStartInfo)
+                .ConfigureAwait(false);
+        }
 
         [Fact]
-        public async Task TestSimpleProjectInfo()
+        public async Task TestCodeAnalysis()
         {
-            using var processResults = await BuildAsync("simple-project-info")
+            using var processResults = await BuildAsync("codeanalysis")
                 .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
 
             using (new AssertionScope())
             {
                 processResults.ExitCode.Should().Be(0);
-                processResults.StandardOutput.Should().Contain("Hello World");
-            }
-        }
-
-        [Fact]
-        public async Task TestSimpleProjectWarning()
-        {
-            using var processResults = await BuildAsync("simple-project-warning")
-                .ConfigureAwait(false);
-
-            using (new AssertionScope())
-            {
-                processResults.ExitCode.Should().Be(0);
-                processResults.StandardOutput.Should().Contain("Hello World");
-            }
-        }
-
-        [Fact]
-        public async Task TestSimpleProjectError()
-        {
-            using var processResults = await BuildAsync("simple-project-error")
-                .ConfigureAwait(false);
-
-            using (new AssertionScope())
-            {
-                processResults.ExitCode.Should().Be(1);
-                processResults.StandardOutput.Should().Contain("Hello World");
             }
         }
 
@@ -54,34 +61,75 @@ namespace GitHubActionsMSBuildLogger.Tests
             using var processResults = await BuildAsync("roslynator")
                 .ConfigureAwait(false);
 
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
+
             using (new AssertionScope())
             {
                 processResults.ExitCode.Should().Be(1);
-                processResults.StandardOutput.Should().Contain("Hello World");
             }
         }
 
         [Fact]
-        public async Task TestCodeAnalysis()
+        public async Task TestSimpleProjectError()
         {
-            using var processResults = await BuildAsync("codeanalysis")
+            using var processResults = await BuildAsync("simple-project-error")
                 .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
+
+            using (new AssertionScope())
+            {
+                processResults.ExitCode.Should().Be(1);
+                processResults.StandardOutput.Should()
+                    .Contain("::error file=TestConsoleApp1/Program.cs,line=13,col=0::; expected");
+            }
+        }
+
+        [Fact]
+        public async Task TestSimpleProjectInfo()
+        {
+            using var processResults = await BuildAsync("simple-project-info")
+                .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
 
             using (new AssertionScope())
             {
                 processResults.ExitCode.Should().Be(0);
-                processResults.StandardOutput.Should().Contain("Hello World");
             }
         }
 
-
-        private async Task<ProcessResults> BuildAsync(string simpleProjectInfo)
+        [Fact]
+        public async Task TestSimpleProjectWarning()
         {
-            var loggerPath = GetLoggerPathOrThrow();
-            var slnPath = GetSolutionPathOrThrow(simpleProjectInfo);
-
-            return await ProcessEx.RunAsync("dotnet", $"build {slnPath} /logger:GitHubActionsLogger,{loggerPath}")
+            using var processResults = await BuildAsync("simple-project-warning")
                 .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
+
+            using (new AssertionScope())
+            {
+                processResults.ExitCode.Should().Be(0);
+                processResults.StandardOutput.Should()
+                    .Contain(
+                        "::warning file=TestConsoleApp1/Program.cs,line=13,col=0::The variable 'hello' is assigned but its value is never used");
+            }
         }
     }
 }
--- src/GitHubActionsMSBuildLogger.Tests/MSBuildTests.cs
+++ src/GitHubActionsMSBuildLogger.Tests/MSBuildTests.cs
@@ -1,58 +1,60 @@
 using System;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using FluentAssertions;
 using FluentAssertions.Execution;
 using RunProcessAsTask;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace GitHubActionsMSBuildLogger.Tests
 {
     public class MsBuildTests : BuildTestsBase
     {
-        private readonly string _msbuildExec;
-
-        public MsBuildTests()
+        public MsBuildTests(ITestOutputHelper output)
         {
+            _output = output;
             _msbuildExec = Environment.GetEnvironmentVariable("GitHubActionsMSBuildLogger_MsBuildPath");
             if (string.IsNullOrEmpty(_msbuildExec)) _msbuildExec = "msbuild";
         }
 
-        [Fact]
-        public async Task TestSimpleProjectInfo()
+        private readonly ITestOutputHelper _output;
+        private readonly string _msbuildExec;
+
+        private async Task<ProcessResults> BuildAsync(string project)
         {
-            using var processResults = await BuildAsync("simple-project-info")
+            var loggerPath = GetLoggerPathOrThrow();
+            var slnPath = GetSolutionPathOrThrow(project);
+
+            var processStartInfo =
+                new ProcessStartInfo(_msbuildExec, $"/logger:GitHubActionsLogger,{loggerPath} {slnPath}")
+                {
+                    Environment =
+                    {
+                        {"GITHUB_ACTION", Guid.NewGuid().ToString()},
+                        {"GITHUB_WORKSPACE", TargetPath}
+                    }
+                };
+
+            return await ProcessEx.RunAsync(processStartInfo)
                 .ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task TestCodeAnalysis()
+        {
+            using var processResults = await BuildAsync("codeanalysis")
+                .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
 
             using (new AssertionScope())
             {
                 processResults.ExitCode.Should().Be(0);
-                processResults.StandardOutput.Should().Contain("Hello World");
-            }
-        }
-
-        [Fact]
-        public async Task TestSimpleProjectWarning()
-        {
-            using var processResults = await BuildAsync("simple-project-warning")
-                .ConfigureAwait(false);
-
-            using (new AssertionScope())
-            {
-                processResults.ExitCode.Should().Be(0);
-                processResults.StandardOutput.Should().Contain("Hello World");
-            }
-        }
-
-        [Fact]
-        public async Task TestSimpleProjectError()
-        {
-            using var processResults = await BuildAsync("simple-project-error")
-                .ConfigureAwait(false);
-
-            using (new AssertionScope())
-            {
-                processResults.ExitCode.Should().Be(1);
-                processResults.StandardOutput.Should().Contain("Hello World");
             }
         }
 
@@ -62,34 +64,76 @@ namespace GitHubActionsMSBuildLogger.Tests
             using var processResults = await BuildAsync("roslynator")
                 .ConfigureAwait(false);
 
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
+
             using (new AssertionScope())
             {
                 processResults.ExitCode.Should().Be(1);
-                processResults.StandardOutput.Should().Contain("Hello World");
             }
         }
 
         [Fact]
-        public async Task TestCodeAnalysis()
+        public async Task TestSimpleProjectError()
         {
-            using var processResults = await BuildAsync("codeanalysis")
+            using var processResults = await BuildAsync("simple-project-error")
                 .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
+
+            using (new AssertionScope())
+            {
+                processResults.ExitCode.Should().Be(1);
+                processResults.StandardOutput.Should()
+                    .Contain("::error file=TestConsoleApp1/Program.cs,line=13,col=0::; expected");
+            }
+        }
+
+
+        [Fact]
+        public async Task TestSimpleProjectInfo()
+        {
+            using var processResults = await BuildAsync("simple-project-info")
+                .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
 
             using (new AssertionScope())
             {
                 processResults.ExitCode.Should().Be(0);
-                processResults.StandardOutput.Should().Contain("Hello World");
             }
         }
 
-        private async Task<ProcessResults> BuildAsync(string project)
+        [Fact]
+        public async Task TestSimpleProjectWarning()
         {
-            var loggerPath = GetLoggerPathOrThrow();
-            var slnPath = GetSolutionPathOrThrow(project);
-
-            return await ProcessEx
-                .RunAsync(_msbuildExec, $"/logger:GitHubActionsLogger,{loggerPath} {slnPath}")
+            using var processResults = await BuildAsync("simple-project-warning")
                 .ConfigureAwait(false);
+
+            var buildOutput = string.Join(Environment.NewLine, processResults.StandardOutput);
+            var errorOutput = string.Join(Environment.NewLine, processResults.StandardError);
+
+            _output.WriteLine($"STDOUT:{Environment.NewLine}{buildOutput}");
+            _output.WriteLine($"STDERR:{Environment.NewLine}{errorOutput}");
+
+            using (new AssertionScope())
+            {
+                processResults.ExitCode.Should().Be(0);
+                processResults.StandardOutput.Should()
+                    .Contain(
+                        "::warning file=TestConsoleApp1/Program.cs,line=13,col=0::The variable 'hello' is assigned but its value is never used");
+            }
         }
     }
 }
